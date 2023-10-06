b3d41ea24184b786830d3d719ce2c6d46b935a2b
mknaefler <mjknaefler@gmail.com>
2023-10-05 22:51:54 -0700

    Add function for finding adults within array

--- functionsChallenge1.js
+++ functionsChallenge1.js
@@ -20,4 +20,24 @@ function reverseString(x){
     newArr = reverseArr.join('');
     return newArr;
 }
+console.log(sampleString);
 console.log(reverseString(sampleString));
+
+//Write a function that takes in an array of objects representing people, where each object has properties for name, age, and gender, and returns an array of objects representing only the people who are over the age of 18.
+let people = [
+    {name: "John Doe", age: 25, gender: "Male"},
+    {name: "Jane Dear", age: 15, gender: "Female"},
+    {name: "Tom Bridge", age: 18, gender: "Male"},
+    {name: "Martha Smith", age: 35, gender:"Female"}
+];
+
+function adults(x){
+    let adultArr = []
+    for (let key of x){
+        if (key.age >= 18){
+            adultArr.push(key)
+        }
+    }
+    return adultArr
+}
+console.log(adults(people));
